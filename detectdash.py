12310fb8c2d1ae48ae1f2a2ac675b6e162f14961
Christoph Pranzl <christoph.pranzl@pranzl.net>
2017-07-18 11:22:20 +0200

    Create detectdash.py

---
+++ detectdash.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+__author__ = "Christoph Pranzl"
+__copyright__ = "Copyright 2016, Christoph Pranzl"
+__credits__ = ["Christoph Pranzl"]
+__license__ = "GPL-2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Christoph Pranzl"
+__email__ = "christoph.pranzl@fasolt.net"
+__status__ = "prototype"
+
+"""
+SYNOPSIS
+
+    detectdash [-h,--help] [-v,--verbose] [--version]
+
+DESCRIPTION
+
+    Detects ARP probing of an Amazon Dashbutton and logs it
+
+EXAMPLES
+
+    detectdash
+
+EXIT STATUS
+
+    TODO: List exit codes
+
+"""
+
+import sys, os, traceback, optparse, subprocess, time
+from datetime import datetime
+from scapy.all import *
+
+lastdetection = 0
+
+
+def logwrite(message):
+
+    logfile = open('logdatei.txt', 'a')
+    logfile.write(message + ' ' + datetime.datetime.utcnow().isoformat() + '\n')
+    logfile.close
+
+
+def arp_display(pkt):
+
+    global lastdetection
+    if pkt.haslayer(DHCP):
+        if pkt[Ether].src == '44:65:0d:8e:93:48':
+            if time.time() - lastdetection > 10:
+                lastdetection = time.time()
+                logwrite("Ariel Button     ")
+        else:
+            logwrite(pkt[Ether].src)
+
+
+def main():
+
+    global options, args
+
+    if options.verbose: logwrite("VERBOSE")
+
+    sniff(prn=arp_display, filter="(udp and (port 67 or 68))", store=0)
+
+
+if __name__ == '__main__':
+    try:
+        start_time = time.time()
+        parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), usage=globals()['__doc__'], versio$
+        parser.add_option ('-m', '--message', dest='message', default='Hello, World!', type='string', help='Message$
+        parser.add_option ('-v', '--verbose', action='store_true', default=False, help='verbose output')
+    (options, args) = parser.parse_args()
+        if options.verbose: logwrite(datetime.utcnow().isoformat())
+        main()
+        if options.verbose: logwrite(datetime.utcnow().isoformat())
+        if options.verbose: logwrite('TOTAL RUNNING TIME IN MINUTES:'),
+        if options.verbose: logwrite((time.time() - start_time) / 60.0)
+        sys.exit(0)
+    except KeyboardInterrupt, e: # Ctrl-C
+        raise e
+    except SystemExit, e: # sys.exit()
+        raise e
+    except Exception, e:
+        print 'ERROR, UNEXPECTED EXCEPTION'
+        print str(e)
+        traceback.print_exc()
+        os.exit(1)
+
+
